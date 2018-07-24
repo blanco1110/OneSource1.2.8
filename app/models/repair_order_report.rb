4486e68bb2acb0abdeed9b344653f7e7fc0269c9
Nabia <nmkalia2@uh.edu>
2018-07-24 02:49:33 -0500

    Updated Repair Order Report totals

--- app/models/repair_order_report.rb
+++ app/models/repair_order_report.rb
@@ -3,10 +3,4 @@ class RepairOrderReport
     RepairOrder.where("repair_order_date >= '#{starting}' and repair_order_date <= '#{ending}'")
   end
 
-  # def self.repair_subtotal(order_subtotal)
-  #   RepairOrder.where("repair_order_subtotal = '#{order_subtotal}'")
-    # query = "Select sum(repair_order_subtotal) FROM RepairOrders"
-    # @sites = ActiveRecord::Base.connection.execute(query)
-    # RepairOrder.where("repair_order_subtotal")
-  # end
 end
