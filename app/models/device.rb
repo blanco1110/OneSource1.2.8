d5835366ca9fe52f4da09cd662336f2059ad5ab6
Terrence Whaley <terrencew1995@gmail.com>
2018-07-29 16:14:40 -0500

    Added number to phone helper on customer show page. Added validation for imei number (min: 15 characters).
    (cherry picked from commit 593c51e)

--- app/models/device.rb
+++ app/models/device.rb
@@ -3,4 +3,7 @@ class Device < ApplicationRecord
   belongs_to :repair_order
   has_many :repair_order_items
   accepts_nested_attributes_for :repair_order_items
+
+  validates :imei_number, length: {minimum: 15}, presence: true
+  validates :device_color, presence: true
 end
