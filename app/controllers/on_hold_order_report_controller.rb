adfc3e4092b0e6f83794e5eaeaa7e22bd57498d1
Nabia <nmkalia2@uh.edu>
2018-08-02 13:52:23 -0500

    Edited the last report pushed

--- app/controllers/on_hold_order_report_controller.rb
+++ app/controllers/on_hold_order_report_controller.rb
@@ -2,7 +2,7 @@ class OnHoldOrderReportController < ApplicationController
   def index
     @repair_orders = OnHoldOrderReport.onholdreport
     on_hold_order_today = OnHoldOrderReport.onholdreport
-    @count_hold_orders = on_hold_order_today.count(:repair_order_date)
+    @count_hold_orders = on_hold_order_today.count(:repair_order_status_id)
     @todaydate = Date.today
 
   end
--- app/controllers/open_order_report_controller.rb
+++ app/controllers/open_order_report_controller.rb
@@ -2,7 +2,7 @@ class OpenOrderReportController < ApplicationController
   def index
     @repair_orders = OpenOrderReport.openorders
     open_order_today = OpenOrderReport.openorders
-    @count_open_orders = open_order_today.count(:repair_order_date)
+    @count_open_orders = open_order_today.count(:repair_order_status_id)
     @todaydate = Date.today
   end
 end
