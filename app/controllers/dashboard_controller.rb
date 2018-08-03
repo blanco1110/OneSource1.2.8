56f6e81471297f195edd08d5317df0ded3803038
slpatton2 <slpatton2@uh.edu>
2018-08-02 20:25:46 -0500

    shwo commit

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,25 @@
 class DashboardController < ApplicationController
   def index
+    @repair_orders = AwaitingPartsReport.awaitingparts
+    awaiting_parts_today = AwaitingPartsReport.awaitingparts
+    @count_awaiting_orders = awaiting_parts_today.count(:repair_order_status_id)
+
+    @repair_orders = NotStartedOrderReport.pendingorders
+    pending_order_today = NotStartedOrderReport.pendingorders
+    @count_pending_orders = pending_order_today.count(:repair_order_status_id)
+
+    @repair_orders = OnHoldOrderReport.onholdreport
+    on_hold_order_today = OnHoldOrderReport.onholdreport
+    @count_hold_orders = on_hold_order_today.count(:repair_order_status_id)
+
+    @repair_orders = OpenOrderReport.openorders
+    open_order_today = OpenOrderReport.openorders
+    @count_open_orders = open_order_today.count(:repair_order_status_id)
+
+    @repair_orders = CompletedOrderReport.completedorders
+    completed_order_today = CompletedOrderReport.completedorders
+    @count_completed_order = completed_order_today.count(:repair_order_status_id)
+
+
   end
 end
