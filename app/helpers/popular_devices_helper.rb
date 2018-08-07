cbce4ff5b80582b29169111a9be30c934e9ae8dd
Nabia <nmkalia2@uh.edu>
2018-08-06 20:27:56 -0500

    Last few edits

--- app/helpers/popular_devices_helper.rb
+++ app/helpers/popular_devices_helper.rb
@@ -8,13 +8,13 @@ module PopularDevicesHelper
   def pop_devices_by_month
     column_chart Device.group_by_month("devices.created_at").joins(:device_version).group("device_versions.device_version_name").count,
                  height: '300px', width: '1200',
-                 xtitle: "Devices", ytitle: "Popularity", title: 'By Week',stacked: true
+                 xtitle: "Devices", ytitle: "Popularity", title: 'By Month',stacked: true
 
   end
   def pop_devices_by_year
     column_chart Device.group_by_year("devices.created_at").joins(:device_version).group("device_versions.device_version_name").count,
                  height: '300px', width: '1200',
-                 xtitle: "Devices", ytitle: "Popularity", title: 'By Week',stacked: true
+                 xtitle: "Devices", ytitle: "Popularity", title: 'By Year',stacked: true
 
   end
 
